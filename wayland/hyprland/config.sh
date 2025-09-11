ac9d77b4d228c4ccd6004dcc01023af97704eb79
Bernd Wachter <bernd-github@wachter.fi>
2025-09-11 12:52:26 +0300

    feat: Add configuration script for hyprland
    Introduce config.sh script for configuring SUSE-based OS images
    using the OpenSuSE KIWI Image System. The script initializes
    machine-specific settings, including generating SSH keys and setting
    up locale information.
    
    Essential functions:
    - Source existing configuration files for environment setup.
    - Generate SSH machine keys if the respective script is available.
    - Remove .mo files from /usr/share/locale to optimize space.
    - Install Hyprland package, setting up necessary desktop components
      and icons through custom scripts.
    - Adjust permissions for /usr/bin/newuidmap and /usr/bin/newgidmap.
    
    This script streamlines the setup for Hyprland, enhancing the OS
    customization process.

---
+++ wayland/hyprland/config.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#================
+# FILE          : config.sh
+#----------------
+# PROJECT       : OpenSuSE KIWI Image System
+# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
+#               :
+# AUTHOR        : Marcus Schaefer <ms@suse.de>
+#               :
+# BELONGS TO    : Operating System images
+#               :
+# DESCRIPTION   : configuration script for SUSE based
+#               : operating systems
+#               :
+#               :
+# STATUS        : BETA
+#----------------
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Create ssh machine keys
+#--------------------------------------
+if [ -f /usr/sbin/sshd-gen-keys-start ]; then
+    /usr/sbin/sshd-gen-keys-start
+fi
+
+(cd /usr/share/locale && find -name "*.mo" | xargs rm)
+
+packages="
+hyprland
+"
+
+for _p in $packages; do
+    /usr/local/bin/desktop-from-package $_p
+    /usr/local/bin/icons-from-package $_p
+done
+
+chmod u+s /usr/bin/newuidmap /usr/bin/newgidmap
